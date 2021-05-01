1269507bf51d46f17230d576d6f489964df58e6b
Roger Johansson <rogeralsing@gmail.com>
2021-05-01 10:30:14 +0200

    first

---
+++ timeout.go
@@ -0,0 +1,80 @@
+package gotimeout
+
+import (
+	"sync"
+	"time"
+)
+
+type TimeoutCallback func()
+
+type timeoutEntry struct {
+	sync.Mutex
+	timestamp time.Time
+	callbacks []TimeoutCallback
+	completed bool
+}
+
+//timeoutEntries expires after 500 milliseconds
+func (te *timeoutEntry) expired() bool {
+	return te.timestamp.Before(time.Now().Add(-time.Duration(500) * time.Millisecond))
+}
+
+func (te *timeoutEntry) AddCallback(callback TimeoutCallback) {
+	if te.completed {
+		return //TODO error? this should not happen so...
+	}
+
+	te.Lock()
+	defer te.Unlock()
+	te.callbacks = append(te.callbacks, callback)
+}
+
+func (te *timeoutEntry) trigger() {
+	te.Lock()
+	defer te.Unlock()
+	te.completed = true
+	for _, callback := range te.callbacks {
+		callback()
+	}
+}
+
+type Timeout struct {
+	entries [60 * 10]*timeoutEntry //we support 10 minutes timeouts with caching, else unique instance
+}
+
+var timeout = &Timeout{}
+
+func AfterFunc(seconds int, callback TimeoutCallback) {
+	timeout.AfterFunc(seconds, callback)
+}
+
+func (t *Timeout) AfterFunc(seconds int, callback TimeoutCallback) {
+	//no timeout, just invoke it
+	if seconds == 0 {
+		callback()
+		return
+	}
+
+	if seconds > len(t.entries)-1 {
+		//just use a unique instance
+		timeout := time.Duration(seconds) * time.Second
+		time.AfterFunc(timeout, callback)
+		return
+	}
+
+	//fetch entry from entry array
+	entry := t.entries[seconds]
+
+	//if entry doesn't exist, or if entry has expired, recreate it
+	if entry == nil || entry.expired() {
+		entry = &timeoutEntry{
+			timestamp: time.Now(),
+		}
+		//this is racy and we don't care, it's OK if it's overwritten, wasting an entry is cheaper than locking
+		t.entries[seconds] = entry
+		timeout := time.Duration(seconds) * time.Second
+		time.AfterFunc(timeout, entry.trigger)
+	}
+
+	entry.AddCallback(callback)
+}
